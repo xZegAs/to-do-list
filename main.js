deaddf3a837516ee668a4fb761c892288c2f0f3c
xZegAs <76397258+xZegAs@users.noreply.github.com>
2024-07-06 16:07:23 +0300

    new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,38 @@
+const table = document.getElementById('list');
+const addItem = document.getElementById('add');
+const clear = document.getElementById('clear');
+const deleteItem = document.getElementById('delete');
+const updateItem = document.getElementById('update');
+const siteName = document.getElementById('name');
+const siteUrl = document.getElementById('url');
+
+let items = [];
+
+addItem.addEventListener('click', () => {
+  if (siteName.value && siteUrl.value) {
+    let item = {
+      name: siteName.value,
+      url: siteUrl.value,
+    };
+    items.push(item);
+    localStorage.setItem('items', JSON.stringify(items));
+
+    items.map((i, index) => {
+        table.innerHTML += `
+        <tr>
+            <td>${index}</td>
+            <td>${i.name}</td>
+            <td>${i.url}</td>
+            <td class="btns">
+                    <button id="update">Update</button>
+                    <button id="delete" onclick="${deleteItems(index)}">Delete</button>
+            </td>
+        </tr>
+        `;
+  });
+
+    
+}
+     siteName.value = '';
+     siteUrl.value = '';
+});
